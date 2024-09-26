db8a9f62d767a681fd09e990fb622c99004f311d
mrunalini <mrunaliniuttam31@gmail.com>
2024-09-26 15:09:10 +0530

    mrunalini's commit

---
+++ SQL TASK query 50rows.sql
@@ -0,0 +1,9 @@
+-- create table E_details(Empid int, FullName varchar,JobTitle varchar,	
+-- Department varchar,	BusinessUnit varchar,Gender varchar,Ethnicity varchar,Age int,HireDate date ,AnnualSalary bigint,Country varchar,City varchar
+
+
+
+
+-- copy E_details from 'D:/SQL TASK/Copy of Excel_Data.csv' DELIMITER ',' csv header;
+
+select * from E_details;
